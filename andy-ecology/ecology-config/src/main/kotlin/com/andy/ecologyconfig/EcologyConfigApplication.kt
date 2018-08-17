df5385fefcedf5096ade2bba3d2ee6244b1623ed
houshuai <houshuai>
2018-08-17 23:32:14 +0800

    增加配置中心

--- andy-ecology/ecology-config/src/main/kotlin/com/andy/ecologyconfig/EcologyConfigApplication.kt
+++ andy-ecology/ecology-config/src/main/kotlin/com/andy/ecologyconfig/EcologyConfigApplication.kt
@@ -2,8 +2,12 @@ package com.andy.ecologyconfig
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.config.server.EnableConfigServer
 
+@EnableConfigServer
 @SpringBootApplication
+@EnableDiscoveryClient
 class EcologyConfigApplication
 
 fun main(args: Array<String>) {
