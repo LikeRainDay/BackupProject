3aa3d49f3a2770e09a3cf24b3355646ce1fa21f4
houshuai <houshuai>
2018-08-17 16:59:46 +0800

    进行更改当前的项目pom 结构

--- andy-ecology/ecology-getway/src/main/kotlin/com/andy/ecologygetway/EcologyGetwayApplication.kt
+++ andy-ecology/ecology-getway/src/main/kotlin/com/andy/ecologygetway/EcologyGetwayApplication.kt
@@ -2,8 +2,10 @@ package com.andy.ecologygetway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class EcologyGetwayApplication
 
 fun main(args: Array<String>) {
