121c732da9d9787afcbc03e6b4a95961922fe5b9
houshuai <houshuai>
2018-08-17 22:38:39 +0800

    1. 采用Consul 进行服务的注册与发现 2. 增加Zuul路由层的 令牌桶限流。

--- andy-ecology/ecology-getway/src/main/kotlin/com/andy/ecologygetway/EcologyGetwayApplication.kt
+++ andy-ecology/ecology-getway/src/main/kotlin/com/andy/ecologygetway/EcologyGetwayApplication.kt
@@ -3,7 +3,9 @@ package com.andy.ecologygetway
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
+@EnableZuulProxy
 @SpringBootApplication
 @EnableDiscoveryClient
 class EcologyGetwayApplication
