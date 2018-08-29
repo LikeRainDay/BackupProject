6d9b49acaae89568f11a1088a7c90448d56501bf
houshuai <houshuai>
2018-08-29 11:45:48 +0800

    删除java 创建的额项目，增加kotlin 创建的额验证码生成项目。

---
+++ andy-server/server-iden_code/src/main/kotlin/com/andy/serveriden_code/ServerIdenCodeApplication.kt
@@ -0,0 +1,11 @@
+package com.andy.serveriden_code
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServerIdenCodeApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServerIdenCodeApplication>(*args)
+}
