80204ecb8041e3fe02027995dd0a68d587c504d9
houshuai <houshuai>
2018-09-02 22:20:48 +0800

    增加用户组信息的实现类

--- andy-server/servier-user-center/src/main/kotlin/com/andy/service/servierusercenter/dao/GroupDao.java
+++ andy-server/servier-user-center/src/main/kotlin/com/andy/service/servierusercenter/dao/GroupDao.java
@@ -3,7 +3,9 @@ package com.andy.service.servierusercenter.dao;
 
 import com.andy.service.servierusercenter.entity.GroupEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -26,10 +28,22 @@ public interface GroupDao extends JpaRepository<GroupEntity, String> {
 
 
     /**
+     * describe: 移除组下所有 下属组
+     * author 候帅
+     * date 2018/9/2 下午9:11
+     * @param orgIndex 索引
+     */
+    void deleteByOrganizeIndexIsStartingWith(String orgIndex);
+
+
+    /**
      * describe: 查询出所有该单位
      * author 候帅
      * date 2018/9/2 下午6:43
      * @param
      * @return
      */
+
+    @Query("select ge from GroupEntity ge where ge.organizeIndex like '?1%'")
+    Optional<List<GroupEntity>> findByOrganizeIndexsLike(String orgIndex);
 }
--- andy-server/servier-user-center/src/main/kotlin/com/andy/service/servierusercenter/service/impl/IGroupServiceImpl.kt
+++ andy-server/servier-user-center/src/main/kotlin/com/andy/service/servierusercenter/service/impl/IGroupServiceImpl.kt
@@ -7,9 +7,11 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.util.StringUtils
 
 @Service
-class IGroupServiceImpl: IGroupService{
+class IGroupServiceImpl : IGroupService {
 
     private val log: Logger = LoggerFactory.getLogger(IGroupServiceImpl::class.java)
 
@@ -17,28 +19,70 @@ class IGroupServiceImpl: IGroupService{
     private lateinit var groupDao: GroupDao
 
     override fun addGroup(groupEntity: GroupEntity): String {
-
-        // TODO 增加对应的组织组信息
-       return groupDao.save(groupEntity).id!!
+        val parentId = groupEntity.parentId
+        val count = groupDao.count() + 1
+        if (StringUtils.isEmpty(parentId)) {
+            groupEntity.organizeIndex = "$count-"
+            groupEntity.organizeLevel = 1
+        } else {
+            val parentEntity = groupDao.findByGroupId(parentId)
+            parentEntity.map {
+                groupEntity.organizeLevel = it.organizeLevel + 1
+                groupEntity.organizeIndex = "${it.organizeIndex}$count-"
+            }.orElseThrow {
+                throw IllegalAccessException("not find $parentId")
+            }
+        }
+        return groupDao.save(groupEntity).id!!
     }
 
+    @Transactional
     override fun deleteGroupById(groupId: String) {
-        groupDao.deleteById(groupId)
+        val groupEntity = groupDao.findByGroupId(groupId)
+        // 移除其下所有匹配的组
+        groupEntity.map {
+            groupDao.deleteByOrganizeIndexIsStartingWith(it.organizeIndex)
+        }.orElseThrow {
+            throw IllegalAccessException("not find $groupId")
+        }
     }
 
     override fun modiftyGroupInfo(groupEntity: GroupEntity) {
-        groupDao.save(groupEntity)
+        val parentId = groupEntity.parentId
+        val group = groupDao.findByGroupId(groupEntity.groupId)
+        group.map {
+            if (it.parentId != parentId)
+                throw RuntimeException("Do not operate to modify superior group information")
+            it.groupDesc = groupEntity.groupDesc
+            it.groupName = groupEntity.groupDesc
+            groupDao.save(it)
+        }.orElseThrow {
+            throw IllegalAccessException("not find ${groupEntity.groupId}")
+        }
     }
 
     override fun modiftyGroupParent(targetId: String, childId: String) {
         val parentEntity = groupDao.findByGroupId(targetId)
         parentEntity.map {
+            val parentId = it.groupId
             val childEntity = groupDao.findByGroupId(childId)
             childEntity.map {
-
-
-
-                it.parentId = targetId
+                val childIndex = it.organizeIndex
+                val split = childIndex.split("-")
+                var preIndex: String? = null
+                for (i in 0 until split.size - 1) {
+                    preIndex += "${split[i]}-"
+                }
+                val preData = groupDao.findByOrganizeIndexsLike(it.organizeIndex)
+                preData.ifPresent {
+                    it.forEach {
+                        // 进行替换当前的父信息
+                        val replace = it.organizeIndex.replace(childIndex, preIndex!!)
+                        it.organizeLevel = replace.split("-").size.toLong()
+                        groupDao.save(it)
+                    }
+                }
+                it.parentId = parentId
                 groupDao.save(it)
             }.orElseThrow {
                 throw IllegalAccessException("Not fond group info")
@@ -51,16 +95,25 @@ class IGroupServiceImpl: IGroupService{
     override fun mergeGroup(targetId: String, childId: String) {
         val parentEntity = groupDao.findByGroupId(targetId)
         parentEntity.map {
-
-
-
-//            val childEntity = groupDao.findByGroupId(childId)
-//            childEntity.map {
-//                it.parentId = targetId
-//                groupDao.save(it)
-//            }.orElseThrow {
-//                throw IllegalAccessException("Not fond group info")
-//            }
+            val organizeIndex = it.organizeIndex
+            val childEntity = groupDao.findByGroupId(childId)
+            childEntity.map {
+                val childIndex = it.organizeIndex
+                val parentId = it.parentId
+                val preData = groupDao.findByOrganizeIndexsLike(it.organizeIndex)
+                preData.ifPresent {
+                    it.forEach {
+                        // 进行替换当前的父信息
+                        val replace = it.organizeIndex.replace(childIndex, organizeIndex!!)
+                        it.organizeLevel = replace.split("-").size.toLong()
+                        if (it.parentId == parentId)
+                            it.parentId = targetId
+                        groupDao.save(it)
+                    }
+                }
+            }.orElseThrow {
+                throw IllegalAccessException("Not fond group info")
+            }
         }.orElseThrow {
             throw IllegalAccessException("Not fond group info")
         }
