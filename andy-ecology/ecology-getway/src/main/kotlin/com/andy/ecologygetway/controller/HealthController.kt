5c2b396a755bb636370335b0909e4d17801c9c7f
18f_100000605004_houshuai0816 <18f_100000605004_houshuai0816@git.cloud.tencent.com>
2018-09-05 21:16:07 +0800

    增加对应的健康检测。

---
+++ andy-ecology/ecology-getway/src/main/kotlin/com/andy/ecologygetway/controller/HealthController.kt
@@ -0,0 +1,20 @@
+package com.andy.ecologygetway.controller
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(value = ["/api"])
+class HealthController {
+
+    private val log: Logger = LoggerFactory.getLogger(HealthController::class.java)
+
+    @GetMapping("/health")
+    fun health(): String {
+        return "ok"
+    }
+
+}
