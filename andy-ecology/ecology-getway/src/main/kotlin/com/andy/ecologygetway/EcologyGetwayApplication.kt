b3946738832b9abdd1d2669375acbbab0ffba04c
houshuai <houshuai>
2018-09-08 00:02:10 +0800

    增加Oauth2的单点登录

--- andy-ecology/ecology-getway/src/main/kotlin/com/andy/ecologygetway/EcologyGetwayApplication.kt
+++ andy-ecology/ecology-getway/src/main/kotlin/com/andy/ecologygetway/EcologyGetwayApplication.kt
@@ -1,6 +1,7 @@
 package com.andy.ecologygetway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
@@ -8,6 +9,8 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 @EnableZuulProxy
 @SpringBootApplication
 @EnableDiscoveryClient
+// 开启OAuth2的单点登录
+@EnableOAuth2Sso
 class EcologyGetwayApplication
 
 fun main(args: Array<String>) {
