2a11ca942e1ff5d60bdb5a735c633fabe56a52df
houshuai <houshuai>
2018-09-20 19:20:26 +0800

    增加java 方法的占位类。打算将 java的dao 放到java对应的目录下进行管理。TODO

---
+++ andy-server/server-user-center/src/main/java/com/andy/service/servicerusercenter/dao/PlaceHolder.java
@@ -0,0 +1,17 @@
+package com.andy.service.servicerusercenter.dao;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 作者：候帅
+ * 时间：2018:09:20-19:19
+ * 描述： 占位类
+ */
+
+public class PlaceHolder {
+
+    private Logger Log = LoggerFactory.getLogger(PlaceHolder.class);
+
+}
