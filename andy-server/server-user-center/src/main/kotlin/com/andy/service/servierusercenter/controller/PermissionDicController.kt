5794c52a2917e4b5eca6aee287c3978a761b558d
houshuai <houshuai>
2018-09-21 09:32:51 +0800

    增加Swagger2的文档注释。

--- andy-server/server-user-center/src/main/kotlin/com/andy/service/servierusercenter/controller/PermissionDicController.kt
+++ andy-server/server-user-center/src/main/kotlin/com/andy/service/servierusercenter/controller/PermissionDicController.kt
@@ -6,6 +6,10 @@ import com.andy.andycommonbean.response.BaseResponse
 import com.andy.andycommonbean.response.ResultResponse
 import com.andy.service.servierusercenter.service.IPmFeatureService
 import com.andy.service.servierusercenter.service.IPmFileService
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiImplicitParam
+import io.swagger.annotations.ApiImplicitParams
+import io.swagger.annotations.ApiOperation
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
@@ -15,9 +19,8 @@ import org.springframework.web.bind.annotation.*
  * describe: 细分后的权限字典添加接口
  * author 候帅
  * date 2018/9/20 下午4:13
- * @param
- * @return
  */
+@Api(value = "PermissionDicController", description = "权限字典")
 @RestController
 @RequestMapping(value = ["/admin/permission/dic"])
 class PermissionDicController : BaseController() {
@@ -31,53 +34,41 @@ class PermissionDicController : BaseController() {
     @Autowired
     private lateinit var iPmFeatureService: IPmFeatureService
 
-    
-    
-    /**
-     * describe: 增加菜单权限字典
-     * author 候帅
-     * data 20/09/2018 21:01
-     * @param
-     * @return
-     */
-    fun addMenuPms(): Unit {
-        
-    }
-    
 
-    /**
-     * describe: 查询文件权限
-     * author 候帅
-     * date 2018/9/20 下午4:45
-     * @param
-     * @return
-     */
+    @ApiOperation(value = "增加菜单权限", notes = "<p>此接口进行对菜单字典的增加")
+    @ApiImplicitParams(
+            ApiImplicitParam(value = "菜单地址", name = "menuUrl", dataType = "String", paramType = "form", required = true),
+            ApiImplicitParam(value = "菜单名字", name = "menuName", dataType = "String", paramType = "form", required = true),
+            ApiImplicitParam(value = "菜单描述", name = "menuDes", dataType = "String", paramType = "form", required = false),
+            ApiImplicitParam(value = "菜单父ID", name = "menuParentId", dataType = "String", paramType = "form", required = false)
+    )
+    @PostMapping(value = ["/menu"])
+    fun addMenuPms(@RequestParam(required = true) menuUrl: String,
+                   @RequestParam(required = true) menuName: String,
+                   @RequestParam menuDes: String,
+                   @RequestParam menuParentId: String): Unit {
+
+    }
+
+
+    @ApiOperation(value = "获取文件权限分页")
+    @ApiImplicitParam(name = "pageRequest", value = "分页设置", required = false, dataType = "PageRequest", paramType = "body")
     @GetMapping(value = ["/file"])
     fun getFilePms(@RequestBody pageRequest: PageRequest): BaseResponse {
         val page = iPmFileService.findPageByParam(pageRequest)
         return ResultResponse.success(page)
     }
 
-    /**
-     * describe: 增加文件权限
-     * author 候帅
-     * date 2018/9/20 下午4:02
-     * @param
-     * @return
-     */
+    @ApiOperation(value = "增加文件权限")
+    @ApiImplicitParam(name = "fileUrl", value = "文件路径", required = true, dataType = "String", paramType = "query")
     @PostMapping(value = ["/file"])
     fun addFilePM(@RequestParam fileUrl: String): BaseResponse {
         val permission = iPmFileService.addFilePermission(fileUrl)
         return ResultResponse.success(permission)
     }
 
-    /**
-     * describe: 删除文件权限
-     * author 候帅
-     * date 2018/9/20 下午4:41
-     * @param
-     * @return
-     */
+    @ApiOperation(value = "删除文件权限")
+    @ApiImplicitParam(name = "fileUrl", value = "文件ID", required = true, dataType = "String", paramType = "path")
     @DeleteMapping(value = ["/file/{fileId}"])
     fun deleteFilePM(@PathVariable fileId: String): BaseResponse {
         iPmFileService.deletePermission(fileId)
